65c1a2c241cdc2ddc74340a14f68ee8991cad3f2
guofeng <feng.guo@chiefclouds.com>
2019-11-08 21:37:13 +0800

    initial

---
+++ src/components/Mheader/Mheader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const name: string = "guofeng";
+
+const Mheader: React.FC = () => {
+    return (
+        <header>
+            <div>
+                {name}
+            </div>
+        </header>
+    )
+}
+export default Mheader;
